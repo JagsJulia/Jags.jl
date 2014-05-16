a8c6af58cb860c79b8ae46925f4a8faccd015485
Rob J. Goedman <goedman@mac.com>
2014-05-16 11:36:19 -0700

    Added .gitignore and Examples/Line directory

---
+++ Examples/Line/line.jl
@@ -0,0 +1,16 @@
+######### Stan batch program example  ###########
+
+#using DataFrames, Distributions, MCMC, Gadfly
+
+old = pwd()
+
+# Probably better to enclose in try...catch construct, ';' sequencing a bit cumbersome
+
+try
+  cd("/Users/rob/.julia/v0.3/MCMCExampleRepository/Jags/")
+  @time run(`jags line.jags`)
+catch e
+  println(e)
+end
+
+cd(old)
