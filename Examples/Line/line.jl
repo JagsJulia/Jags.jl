77fd942679017078e30b2273b3639db5426808cc
Rob Goedman <goedman@mac.com>
2014-07-29 21:06:13 -0700

    Renamed Line2 to Line

--- Examples/Line/line.jl
+++ Examples/Line/line.jl
@@ -3,26 +3,45 @@
 using Jags
 
 old = pwd()
-ProjDir = homedir()*"/.julia/v0.3/Jags/Examples/Line"
+ProjDir = homedir()*"/.julia/v0.3//Jags/Examples/Line2"
 
-idx = 0
-samples = 0
-try
-  cd(ProjDir)
-  for i in 1:4
-    isfile("CODAchain$(i).txt") && rm("CODAchain$(i).txt")
-  end
-  isfile("CODAindex.txt") && rm("CODAindex.txt")
-  
-  @time run(`jags line.jags`)
-  (idx, samples) = read_jagsfiles()
+line = "
+model {
+  for (i in 1:n) {
+        mu[i] <- alpha + beta*(x[i] - x.bar);
+        y[i]   ~ dnorm(mu[i],tau);
+  }
+  x.bar   <- mean(x[]);
+  alpha    ~ dnorm(0.0,1.0E-4);
+  beta     ~ dnorm(0.0,1.0E-4);
+  tau      ~ dgamma(1.0E-3,1.0E-3);
+  sigma   <- 1.0/sqrt(tau);
+}
+"
 
-catch e
-  println(e)
-  cd(old)
-end
+data = Dict{Symbol, Any}()
+data[:x] = [1, 2, 3, 4, 5]
+data[:y] = [1, 3, 3, 3, 5]
+data[:n] = 5
+
+inits = (Symbol => Any)[
+  :alpha => 0,
+  :beta => 0,
+  :tau => 1
+]
+
+jagsmodel = Jagsmodel(name="line", model=line, data=data, init=inits)
+(idx, chains) = fit(jagsmodel, ProjDir)
 
 println()
-samples[1][:samples] |> display
+data |> display
+println()
+inits |> display
+println()
+idx |> display
+println()
+jagsmodel |> display
+println()
+chains[1][:samples] |> display
 
 cd(old)
