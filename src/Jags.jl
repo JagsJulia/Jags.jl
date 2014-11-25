703e308fa527ed7f7663253ad0870e8ab691cec5
Rob J Goedman <goedman@mac.com>
2014-11-25 08:10:34 -0800

    Removed export of JAGS_HOME and JULIA_SVG_BROWSER

--- src/Jags.jl
+++ src/Jags.jl
@@ -12,17 +12,17 @@ module Jags
   end
   
   if !isdefined(Main, :JAGS_HOME)
-    JAGS_HOME = ""
+    global JAGS_HOME = ""
     try
-      JAGS_HOME = ENV["JAGS_HOME"]
+      global JAGS_HOME = ENV["JAGS_HOME"]
     catch e
       println("Environment variable JAGS_HOME not found.")
     end
   end
   if !isdefined(Main, :JULIA_SVG_BROWSER)
-    JULIA_SVG_BROWSER = ""
+    global JULIA_SVG_BROWSER = ""
     try
-      JULIA_SVG_BROWSER = ENV["JULIA_SVG_BROWSER"]
+      global JULIA_SVG_BROWSER = ENV["JULIA_SVG_BROWSER"]
     catch e
       println("Environment variable JULIA_SVG_BROWSER not found.")
       println("Produced .svg files in examples will not be automatically displayed.")
@@ -33,8 +33,8 @@ module Jags
   
   export
   # From Jags.jl
-    JAGS_HOME,
-    JULIA_SVG_BROWSER,
+    #JAGS_HOME,
+    #JULIA_SVG_BROWSER,
     
   # From jagsmodel.jl
     Jagsmodel,
