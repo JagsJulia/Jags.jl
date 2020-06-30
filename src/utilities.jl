5f8bd7f5740b105e4b9c856441ad612b948ba55f
Lars Hellemo <lars.hellemo@sintef.no>
2020-06-30 16:45:31 +0200

    Simplified/minor speedup

--- src/utilities.jl
+++ src/utilities.jl
@@ -82,6 +82,6 @@ function par(cmd::Array{String, 1})
 end
 
 function readdlmcsv(fn)
-    tmp = CSV.File(fn; header=0,delim=' ',ignorerepeated=true,type=Float64)
-    return collect(hcat(collect(tmp.Column1),collect(tmp.Column2)))
+  tmp = CSV.File(fn; header=0, delim=' ', ignorerepeated=true, type=Float64)
+  return hcat(tmp.Column1, tmp.Column2)
 end
